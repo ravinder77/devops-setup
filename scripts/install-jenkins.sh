aafee894e8a8384d2218c91ed6ad7311d77530db
Ravinder Singh <77714508+ravinder77@users.noreply.github.com>
2026-03-25 13:20:50 +0530

    update script

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/rpm-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key
-sudo yum upgrade
-sudo yum install java-21-amazon-corretto -y
-sudo yum install jenkins -y
+
+# ubuntu
+#install java
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
+
+
+# Amazon linux
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo dnf upgrade
+# Add required dependencies for the Jenkins package
+sudo dnf install fontconfig java-21-openjdk
+sudo dnf install jenkins
+sudo systemctl daemon-reload
+
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+
